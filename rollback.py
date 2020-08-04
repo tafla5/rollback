61e9b8cabd1c2f6039d2f318fc7b1ba6768a57c6
tafla5 <34250995+tafla5@users.noreply.github.com>
2020-08-04 01:13:27 -0500

    Add files via upload
    First version

---
+++ rollback.py
@@ -0,0 +1,48 @@
+import re
+
+def no_object(line):
+    single_object[interation].append('no '+line)
+def no_access_list(line):
+    #szuka line i cyfr a nastepnie jest zamienia na pusty znak
+    without_line = re.sub(' line [1-9]+','',line)
+    access_list[interation].append('no '+without_line)
+def no_object_group(line):
+    object_group[interation].append('no '+line)
+
+file_name = input('Provide source file name: ')
+
+single_object = [[],[]]
+object_group = [[],[]]
+access_list = [[],[]]
+ssh = [[],[]]
+a = -1
+interation = -1
+
+with open(file_name+'.txt', 'r+') as file_object:
+    for line in file_object:
+        if line.startswith('ssh'):
+            interation+=1
+            ssh[interation].append(line)
+        elif line.startswith('conf'):
+            ssh[interation].append(line)
+        elif line.startswith('object-group'):
+            no_object_group(line)
+        elif line.startswith('object '):
+            no_object(line)
+        elif line.startswith('access-list'):
+            no_access_list(line)
+
+
+with open(file_name+'.txt', 'a') as file_object:
+    file_object.write('\n\n###########################\n\n\n\n')
+    while a < interation:
+        a+=1
+        for i in ssh[a]:
+            file_object.write(str(i)+'\n')
+        for i in access_list[a]:
+            file_object.write(str(i))
+        for i in object_group[a]:
+            file_object.write(str(i))
+        for i in single_object[a]:
+            file_object.write(str(i))
+        file_object.write("\nend \nwr\n\n")
